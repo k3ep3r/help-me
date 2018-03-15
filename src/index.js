07578c877574ae47f364174c98ec7abbcb624bec
Sergey Andreev <keeper3211@gmail.com>
2018-03-15 19:50:59 +0300

    6 Passed, havnt time for more

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function count(s, pairs) {
-  // your implementation
+    let result = 1;
+    if (s == "1" || s == "01"){
+        for (let i = 0; i < pairs.length; i++) {
+            result *= (pairs[i][0] - 1);
+        }
+        return result % 1000000007;
+    }
+    return result % 1000000007;
 }
